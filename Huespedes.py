dcabb01745b145c8248c8654dacdce618213ae9b
Matias Bernal <matrekb@gmail.com>
2023-10-02 13:27:29 -0300

    clase Huespedes

---
+++ Huespedes.py
@@ -0,0 +1,19 @@
+class huespedes:
+    def __init__(self,nombre,apellido,dni,idioma,nacionalidad):
+        self.__nombre=nombre
+        self.__apellido=apellido
+        self.__dni=dni
+        self.__idioma=idioma
+        self.__nacionalidad=nacionalidad
+
+    '''
+    def hacerReserva(self):
+   
+    def cancelarReserva(self):
+
+    def hacerActividades(self):
+
+    def cancelarActividades(self):
+    '''
+
+
