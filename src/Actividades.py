cac23a599b18bc3d58fbe0f774c5e60c0a47cb9c
Matias Bernal <matrekb@gmail.com>
2023-10-02 13:46:57 -0300

    clase Actividades

---
+++ src/Actividades.py
@@ -0,0 +1,7 @@
+class Actividades:
+    def __innit__(self, nombre, tipo, precio, fecha, idioma):
+        self.__nombre = nombre
+        self.__tipo = tipo
+        self.__precio = precio
+        self.__fecha = fecha
+        self.__idioma = idioma
