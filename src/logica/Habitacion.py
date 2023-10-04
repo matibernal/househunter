fcb6a1f3db925d1f1c02d0dad7b67df85531889d
Matias Bernal <matrekb@gmail.com>
2023-10-04 17:57:33 -0300

    login en Main y lista en Habitacion class

--- src/logica/Habitacion.py
+++ src/logica/Habitacion.py
@@ -1,5 +1,10 @@
 class Habitacion:
+
+    habitaciones=[]
     def __init__(self, precio, numero):
         self.__precio = precio
         self.__numero = numero
+        Habitacion.habitaciones.append(self)
 
+    def verHabitaciones(self, habitaciones):
+        print("Habitaciones: ", habitaciones)
---
+++ src/main.py
@@ -0,0 +1,49 @@
+from logica.Habitacion import Habitacion
+
+print("Bienvenido al sistema")
+
+while True:
+    opciones=int(input('''¿Que desea hacer?
+          1- Crear habitacion
+          2- Ver habitaciones disponibles
+          3- Hacer una reserva
+          4- Cancelar una reserva
+          5- Crear actividades
+          6- Asignar actividades
+          7- Cancelar actividades
+          8- Salir del sistema'''))
+
+    if opciones == 1:
+        precio = float(input("Ingrese el costo del alquiler de la habitacion: "))
+        numero = int(input("Ingrese el numero de la habitacion: "))
+        habitacionNueva = Habitacion(precio, numero)
+        print("La habitacion fue creada")
+
+    elif opciones == 2:
+        # Realizar acción para la opción 2
+        pass
+    elif opciones == 3:
+        # Realizar acción para la opción 3
+        pass
+    elif opciones == 4:
+        pass
+    elif opciones == 5:
+        pass
+    elif opciones == 6:
+        pass
+    elif opciones == 7:
+        pass
+    elif opciones == 8:
+        print("Usted ha salido del sistema, muchas gracias")
+        break
+    else:
+        print("La opcion ingresada no es valida")
+
+'''error = 0
+while error == 0:
+    try:
+        numero=int(input("Ingrese numero"))
+        error=1
+        print("Final")
+    except ValueError:
+        print("Error al escribir numero")'''
