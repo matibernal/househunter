30b5ba5ee00474bb8502971a54f72ac6a9a0ed03
Matias Bernal <matrekb@gmail.com>
2023-10-19 01:55:51 -0300

    bdd y habitaciones en bdd

---
+++ src/bdd/bd.py
@@ -0,0 +1,54 @@
+import sqlite3
+
+class Conexion:
+    def __init__(self, nombreBD):
+        self.nombreBD = nombreBD
+        self.conexion = sqlite3.connect(nombreBD)
+        self.cursor = self.conexion.cursor()
+
+##HABITACIONES
+    def crearTablaHabitaciones(self):
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS HABITACION("
+            "id INTEGER PRIMARY KEY,"
+            "precio float,"
+            "capacidad int)"
+        )
+        self.conexion.commit()
+
+    def insertarHabitacion(self, precio, capacidad):
+        self.cursor.execute(
+            "INSERT INTO HABITACION(precio, capacidad) VALUES (?,?)",
+            (precio,capacidad))
+        self.conexion.commit()
+
+    def MostrarHabitaciones(self):
+        self.cursor.execute("SELECT * FROM HABITACION")
+        habitacion = self.cursor.fetchall()
+        return habitacion
+
+    def EditarHabitacion(self, id, precio, capacidad):
+        self.cursor.execute(
+            " UPDATE HABITACION SET precio=? , capacidad=? WHERE id=?",
+            (precio, capacidad, id))
+        self.conexion.commit()
+
+    def EliminarHabitacion(self, id):
+        self.cursor.execute(" DELETE FROM HABITACION WHERE id=?  ", (id))
+        self.conexion.commit()
+
+    def FiltrarHabitaciones(self, precio, capacidad):
+        self.cursor.execute("SELECT * FROM HABITACION WHERE precio <= ? AND capacidad >= ?",
+                            (precio, capacidad))
+        habitaciones_filtradas = self.cursor.fetchall()
+
+
+        for habitacion in habitaciones_filtradas:
+            print("Numero de habitación:", habitacion[0])
+            print("Precio de la habitacion:", habitacion[1])
+            print("Capacidad de la habitacion:", habitacion[2])
+            print()
+##FIN DE HABITACIONES
+    def CerrarConexion(self):
+        self.cursor.close()
+        self.conexion.close()
--- src/main.py
+++ src/main.py
@@ -1,53 +1,91 @@
 from datetime import datetime
 from logica.Habitacion import Habitacion
 from logica.Actividades import Actividades
-print("Bienvenido al sistema")
+import sqlite3
+from bdd.bd import Conexion
 
-while True:
-    opciones=int(input('''¿Que desea hacer?
-          1- Crear habitacion
-          2- Ver habitaciones disponibles
-          3- Hacer una reserva
-          4- Cancelar una reserva
-          5- Crear actividades
-          6- Ver actividades
-          7- Asignar actividades
-          8- Cancelar actividades
-          9- Salir del sistema'''))
 
-    if opciones == 1:
-        precio = float(input("Ingrese el costo del alquiler de la habitacion: "))
-        numero = int(input("Ingrese el numero de la habitacion: "))
-        habitacionNueva = Habitacion(precio, numero)
-        print("La habitacion fue creada")
-
-    elif opciones == 2:
-        Habitacion.verHabitaciones()
-    elif opciones == 3:
-        pass
-    elif opciones == 4:
-        pass
-    elif opciones == 5:
-        nombre = input("Ingrese el nombre de la actividad: ")
-        tipo = input("Ingrese el tipo de actividad: ")
-        precio = float(input("Ingrese el precio de la actividad: "))
-        fechastr = input("Ingrese la fecha y hora de la actividad con el formato 'YYYY-MM-DD HH:MM:SS': ")
+if __name__ == "__main__":
+    print("Bienvenido al sistema")
+    nombreBD = "houseHounter.db"
+    conexion = Conexion(nombreBD)
+    conexion.crearTablaHabitaciones()
+    error = 1
+    opciones = 0
+    while error!= 0:
         try:
-            fecha = datetime.strptime(fechastr, "%Y-%m-%d %H:%M:%S")
-            print("Fecha de la actividad: ", fecha)
-        except ValueError:
-            print("El formato de la fecha ingresada no es valido, use 'YYYY-MM-DD HH:MM:SS' por favor")
-        idioma = input("Ingrese el idioma de la actividad: ")
-        actividadNueva = Actividades(nombre, tipo, precio, fecha, idioma)
+            opciones = int(input('''¿Que desea hacer?
+                          1- Crear habitacion
+                          2- Ver habitaciones disponibles
+                          3- Editar habitaciones
+                          4- Eliminar habitaciones
+                          5- Buscar habitaciones por filtro
+                          6- Crear actividades
+                          7- Ver actividades
+                          8- Cancelar actividades
+                          9- Salir del sistema'''))
 
-    elif opciones == 6:
-        Actividades.verActividades()
-    elif opciones == 7:
-        pass
-    elif opciones == 8:
-        print("Usted ha salido del sistema, muchas gracias")
-        break
-    else:
-        print("La opcion ingresada no es valida")
+        except ValueError:
+            print("La opcion ingresada no es valida")
+            opciones = 0
+
+        if opciones == 1:
+            precio = float(input("Ingrese el costo del alquiler de la habitacion: "))
+            capacidad = int(input("Ingrese la capacidad de la habitacion"))
+            conexion.insertarHabitacion(precio,capacidad)
+            print("La habitacion fue creada")
+
+        elif opciones == 2:
+            habitaciones = conexion.MostrarHabitaciones()
+
+            if len(habitaciones) > 0:
+                print("Lista de habitaciones")
+                for habitacion in habitaciones:
+                    print(
+                        f"Precio: {habitacion[1]}, Capacidad: {habitacion[2]} , Numero: {habitacion[0]}"
+                    )
+
+            else:
+                print("No hay habitaciones creadas")
+        elif opciones == 3:
+            id = input("Ingrese el numero de la habitacion a editar")
+            precio = input("Ingrese el precio de la habitacion")
+            capacidad = input("Ingrese la capacidad de la habitacion")
+            conexion.EditarHabitacion(id, precio, capacidad)
+            print("La habitacion fue editada con exito")
+
+        elif opciones == 4:
+            id = input("Ingrese el id de la habitacion para eliminarla")
+            conexion.EliminarHabitacion(id)
+            print("La habitacion fue eliminada con exito")
+
+        elif opciones == 5:
+            precio_max = float(input("Ingrese el precio máximo que busca en una habitacion"))
+            capacidad_min = int(input("Ingrese la capacidad minima que busca en una habitacion"))
+            conexion.FiltrarHabitaciones(precio_max,capacidad_min)
+
+
+        elif opciones == 6:
+            nombre = input("Ingrese el nombre de la actividad: ")
+            tipo = input("Ingrese el tipo de actividad: ")
+            precio = float(input("Ingrese el precio de la actividad: "))
+            fechastr = input("Ingrese la fecha y hora de la actividad con el formato 'YYYY-MM-DD HH:MM:SS': ")
+            try:
+                fecha = datetime.strptime(fechastr, "%Y-%m-%d %H:%M:%S")
+                print("Fecha de la actividad: ", fecha)
+            except ValueError:
+                print("El formato de la fecha ingresada no es valido, use 'YYYY-MM-DD HH:MM:SS' por favor")
+            idioma = input("Ingrese el idioma de la actividad: ")
+            actividadNueva = Actividades(nombre, tipo, precio, fecha, idioma)
+
+        elif opciones == 7:
+            Actividades.verActividades()
+        elif opciones == 8:
+            pass
+        elif opciones == 9:
+            print("Usted ha salido del sistema, muchas gracias")
+            conexion.CerrarConexion()
+        else:
+            print("La opcion ingresada no es valida")
 
 
