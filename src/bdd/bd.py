29ce702698f2fb78fe40e72016d01d0e83236ac6
Matias Bernal <matrekb@gmail.com>
2023-11-02 05:29:22 -0300

    Funciones actividad y huesped con validaciones

--- src/bdd/bd.py
+++ src/bdd/bd.py
@@ -34,7 +34,7 @@ class Conexion:
         self.conexion.commit()
 
     def EliminarHabitacion(self, id):
-        self.cursor.execute(" DELETE FROM HABITACION WHERE id=?  ", (id))
+        self.cursor.execute("DELETE FROM HABITACION WHERE id=?", (id,))
         self.conexion.commit()
 
     def FiltrarHabitaciones(self, precio, capacidad):
@@ -42,14 +42,368 @@ class Conexion:
                             (precio, capacidad))
         habitaciones_filtradas = self.cursor.fetchall()
 
-
-        for habitacion in habitaciones_filtradas:
-            print("Numero de habitación:", habitacion[0])
-            print("Precio de la habitacion:", habitacion[1])
-            print("Capacidad de la habitacion:", habitacion[2])
-            print()
+        if not habitaciones_filtradas:
+            print("No se encontraron habitaciones que cumplan el filtro")
+        else:
+            for habitacion in habitaciones_filtradas:
+                print("Numero de habitación:", habitacion[0])
+                print("Precio de la habitacion:", habitacion[1])
+                print("Capacidad de la habitacion:", habitacion[2])
+                print()
 
 ##FIN DE HABITACIONES
+    ##HUESPED
+    def crearTablaHuesped(self):
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS HUESPED("
+            "id INTEGER PRIMARY KEY,"
+            "nombre text,"
+            "apellido text,"
+            "dni int,"
+            "edad int,"
+            "sexo text,"
+            "idioma text,"
+            "nacionalidad text)"
+        )
+        self.conexion.commit()
+
+    def InsertarHuesped(self, nombre, apellido, dni, edad, sexo, idioma, nacionalidad):
+        self.cursor.execute(
+            "INSERT INTO HUESPED (nombre, apellido, dni, edad, sexo, idioma, nacionalidad) VALUES (?,?, ?, ?, ?, ?, ?)",
+            (nombre, apellido, dni, edad, sexo, idioma, nacionalidad))
+        self.conexion.commit()
+
+    def MostrarHuesped(self):
+        self.cursor.execute("SELECT * FROM HUESPED")
+        huesped = self.cursor.fetchall()
+        return huesped
+
+    def EditarHuesped(self, id, nombre, apellido, dni, edad, sexo, idioma, nacionalidad):
+        self.cursor.execute(
+            " UPDATE HUESPED SET nombre=? , apellido=?, dni=? , edad=?, sexo=? , idioma=?, nacionalidad=? WHERE id=?",
+            (nombre, apellido, dni, edad, sexo, idioma, nacionalidad, id))
+        self.conexion.commit()
+
+    def EliminarHuesped(self, id):
+        self.cursor.execute("SELECT id FROM HUESPED WHERE id=?", (id,))
+        resultado = self.cursor.fetchone()
+
+        if resultado:
+            self.cursor.execute("DELETE FROM HUESPED WHERE id=?", (id,))
+            self.conexion.commit()
+            print("El huesped fue eliminado con éxito")
+        else:
+            print("El ID ingresado no existe, no se pudo eliminar al huesped")
+    ##falta filtro por huesped
+
+    ##NOMBRE DE LAS HABITACIONES --- ## agregar los insert a mano
+    def crearTablaNombreHabitaciones(self):
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS NOMBRE_HABITACIONES("
+            "id INTEGER PRIMARY KEY,"
+            "nombre text,"
+            "sexo text)"
+        )
+        self.conexion.commit()
+
+    def InsertarNombreHabitaciones(self, nombre, sexo):
+        self.cursor.execute(
+            "INSERT INTO NOMBRE_HABITACIONES(nombre, sexo) VALUES (?,?)",
+            (nombre, sexo))
+        self.conexion.commit()
+
+    def MostrarNombreHabitaciones(self):
+        self.cursor.execute("SELECT * FROM NOMBRE_HABITACIONES")
+        nombre_habitaciones = self.cursor.fetchall()
+        return nombre_habitaciones
+
+    def EditarNombreHabitaciones(self, id, precio, capacidad):
+        self.cursor.execute(
+            " UPDATE NOMBRE_HABITACIONES SET nombre=? , sexo=? WHERE id=?",
+            (precio, capacidad, id))
+        self.conexion.commit()
+
+    def EliminarNombreHabitaciones(self, id):
+        self.cursor.execute(" DELETE FROM NOMBRE_HABITACIONES WHERE id=?  ", (id))
+        self.conexion.commit()
+
+    ## filtro de tipos de reservas
+
+    ##RECEPCIONISTA -- ver si creamos empleado o recepcionista y si puede fiChar o no
+    def crearTablaRecepcionista(self):
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS RECEPCIONISTA("
+            "id INTEGER PRIMARY KEY,"
+            "dni int,"
+            "nombre text,"
+            "apellido text,"
+            "sueldo float)"
+        )
+        self.conexion.commit()
+
+    def InsertarRecepcionista(self, dni, nombre, apellido, sueldo):
+        self.cursor.execute(
+            "INSERT INTO RECEPCIONISTA(dni, nombre, apellido, sueldo) VALUES (?,?)",
+            (dni, nombre, apellido, sueldo))
+        self.conexion.commit()
+
+    def MostrarRecepcionista(self):
+        self.cursor.execute("SELECT * FROM RECEPCIONISTA")
+        recepcionista = self.cursor.fetchall()
+        return recepcionista
+
+    def EditarNombreRecepcionista(self, id, precio, capacidad):
+        self.cursor.execute(
+            " UPDATE RECEPCIONISTA SET dni=? , nombre=?, apellido=?, sueldo=? WHERE id=?",
+            (precio, capacidad, id))
+        self.conexion.commit()
+
+    def EliminarRecepcionista(self, id):
+        self.cursor.execute(" DELETE FROM RECEPCIONISTA WHERE id=?  ", (id))
+        self.conexion.commit()
+
+    ##filtrar recepcionistas por sueldo?
+
+    ##FICHAR
+    def crearTablaFichaje(self):
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS FICHAJE("
+            "id INTEGER PRIMARY KEY,"
+            "fecha date,"
+            "hora_ingreso time,"
+            "hora_salida time,"
+            "ID_Recepcionista int FOREIGN KEY)"
+        )
+        self.conexion.commit()
+
+    def InsertarFichaje(self, fecha, hora_ingreso, hora_salida, ID_Recepcionista):
+        self.cursor.execute(
+            "INSERT INTO FICHAJE(fecha, hora_ingreso, hora_salida, ID_Recepcionista) VALUES (?,?, ?, ?)",
+            (fecha, hora_ingreso, hora_salida, ID_Recepcionista))
+        self.conexion.commit()
+
+    def MostrarFichaje(self):
+        self.cursor.execute("SELECT * FROM FICHAJE")
+        fichaje = self.cursor.fetchall()
+        return fichaje
+
+    def EditarFichaje(self, id, fecha, hora_ingreso, hora_salida, ID_Recepcionista):
+        self.cursor.execute(
+            " UPDATE FICHAJE SET fecha=? , hora_ingreso=?, hora_salida=?, ID_Recepcionista=? WHERE id=?",
+            (fecha, hora_ingreso, hora_salida, ID_Recepcionista, id))
+        self.conexion.commit()
+
+    def EliminarFichaje(self, id):
+        self.cursor.execute(" DELETE FROM FICHAJE WHERE id=?  ", (id))
+        self.conexion.commit()
+
+    ## filtrar fichajes por fecha
+
+    ##FORMA RESERVA ##agregar insert a mano
+    def crearTablaForma_Reserva(self):
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS FORMA_RESERVA("
+            "id INTEGER PRIMARY KEY,"
+            "nombre text)"
+        )
+        self.conexion.commit()
+
+    def InsertarForma_Reserva(self, nombre):
+        self.cursor.execute(
+            "INSERT INTO FORMA_RESERVA(nombre) VALUES (?)",
+            (nombre))
+        self.conexion.commit()
+
+    def MostrarForma_Reserva(self):
+        self.cursor.execute("SELECT * FROM FORMA_RESERVA")
+        forma_reserva = self.cursor.fetchall()
+        return forma_reserva
+
+    def EditarForma_Reserva(self, nombre):
+        self.cursor.execute(
+            " UPDATE FORMA_RESERVA SET nombre=? WHERE id=?",
+            (nombre, id))
+        self.conexion.commit()
+
+    def EliminarForma_Reserva(self, id):
+        self.cursor.execute(" DELETE FROM FORMA_RESERVA WHERE id=?  ", (id))
+        self.conexion.commit()
+
+    ##RESERVA
+    def crearTablaTiposReserva(self):
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS TIPOS_RESERVA("
+            "id INTEGER PRIMARY KEY,"
+            "tipo int,"
+            "descripcion text)"
+        )
+        self.conexion.commit()
+
+    def InsertarTiposReserva(self, precio, capacidad):
+        self.cursor.execute(
+            "INSERT INTO TIPOS_RESERVA(tipo, descripcion) VALUES (?,?)",
+            (precio, capacidad))
+        self.conexion.commit()
+
+    def MostrarTiposReserva(self):
+        self.cursor.execute("SELECT * FROM TIPOS_RESERVA")
+        tipos_reserva = self.cursor.fetchall()
+        return tipos_reserva
+
+    def EditarTiposReserva(self, id, precio, capacidad):
+        self.cursor.execute(
+            " UPDATE TIPOS_RESERVA SET tipo=? , descripcion=? WHERE id=?",
+            (precio, capacidad, id))
+        self.conexion.commit()
+
+    def EliminarTiposReserva(self, id):
+        self.cursor.execute(" DELETE FROM TIPOS_RESERVA WHERE id=?  ", (id))
+        self.conexion.commit()
+
+    ## falta filtro de reserva
+
+    ##RESERVA HABITACION
+    def crearTablaReservaHabitacion(self):
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS RESERVA_HABITACION("
+            "id INTEGER PRIMARY KEY,"
+            "desde date,"
+            "hasta date,"
+            "check_in time,"
+            "chek_out time,"
+            "cantidad_personas int,"
+            "terminada boolean,"
+            "llave_entregada boolean DEFAULT 0,"
+            "confirmada boolean DEFAULT 0,"
+            "pago float,"
+            "pago_realizado boolean,"
+            "cancelada boolean DEFAULT 0,"
+            "ID_huesped int,"
+            "ID_habitacion int,"
+            "ID_formareserva int,"
+            "FOREIGN KEY (ID_huesped) REFERENCES HUESPED(id),"
+            "FOREIGN KEY (ID_habitacion) REFERENCES HABITACION(id))"
+        )
+        self.conexion.commit()
+
+    def InsertarReservaHabitacion(self, desde, hasta, check_in, chek_out, terminada, llave_entregada, confirmada, pago,
+                                  pago_realizado, cancelada, ID_huesped, ID_habitacion, ID_formareserva):
+        self.cursor.execute(
+            "INSERT INTO RESERVA_HABITACION (desde, hasta, check_in, chek_out, terminada, llave_entregada, confirmada, pago, pago_realizado, cancelada, ID_huesped, ID_habitacion, ID_formareserva) VALUES (?,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            (desde, hasta, check_in, chek_out, terminada, llave_entregada, confirmada, pago, pago_realizado, cancelada,
+             ID_huesped, ID_habitacion, ID_formareserva))
+        self.conexion.commit()
+
+    def MostrarReservaHabitacion(self):
+        self.cursor.execute("SELECT * FROM RESERVA_HABITACION")
+        reserva_habitacion = self.cursor.fetchall()
+        return reserva_habitacion
+
+    def EditarReservaHabitacion(self, id, desde, hasta, check_in, chek_out, terminada, llave_entregada, confirmada,
+                                pago, pago_realizado, cancelada, ID_huesped, ID_habitacion, ID_formareserva):
+        self.cursor.execute(
+            " UPDATE HABITACION SET desde=? , hasta=?, check_in=? , chek_out=?, terminada=? , llave_entregada=?, confirmada=? , pago=?, pago_realizado=?, cancelada=?, ID_huesped=?, ID_habitacion=? , ID_formareserva=? WHERE id=?",
+            (desde, hasta, check_in, chek_out, terminada, llave_entregada, confirmada, ID_huesped, pago, pago_realizado,
+             cancelada, ID_habitacion, ID_formareserva, id))
+        self.conexion.commit()
+
+    def EliminarReservaHabitacion(self, id):
+        self.cursor.execute(" DELETE FROM HABITACION WHERE id=?  ", (id))
+        self.conexion.commit()
+
+    ##falta filtro de las reservas de las habitaciones
+
+    ##ACTIVIDADES -- ## agregar los insert a mano
+
+    def crearTablaActividad(self):
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS ACTIVIDAD("
+            "id INTEGER PRIMARY KEY,"
+            "actividad text,"
+            "precio float,"
+            "cupos int,"
+            "cant_reservas int,"
+            "disponible boolean DEFAULT 1)"
+        )
+        self.conexion.commit()
+
+    def InsertarActividad(self, actividad, precio, cupos, cant_reservas, disponible):
+        self.cursor.execute(
+            "INSERT INTO ACTIVIDAD (actividad, precio, cupos, cant_reservas, disponible) VALUES (?, ?, ?, ?, ?)",
+            (actividad, precio, cupos, cant_reservas, disponible))
+        self.conexion.commit()
+
+    def MostrarActividad(self):
+        self.cursor.execute("SELECT * FROM ACTIVIDAD")
+        actividad = self.cursor.fetchall()
+        return actividad
+
+    def EditarActividad(self, id, actividad, precio, cupos, cant_reservas, disponible):
+        self.cursor.execute(
+            " UPDATE ACTIVIDAD SET actividad=? , precio=?, cupos=? , cant_reservas=?, disponible=? WHERE id=?",
+            (actividad, precio, cupos, cant_reservas, disponible, id))
+        self.conexion.commit()
+
+    def EliminarActividad(self, id):
+        self.cursor.execute("SELECT id FROM ACTIVIDAD WHERE id=?", (id,))
+        resultado = self.cursor.fetchone()
+
+        if resultado:
+            self.cursor.execute("DELETE FROM ACTIVIDAD WHERE id=?", (id,))
+            self.conexion.commit()
+            print("La actividad fue eliminada con éxito")
+        else:
+            print("El ID ingresado no existe, no se pudo eliminar la actividad")
+
+    ##falta filtro de las actvidades
+    def crearTablaReservaActividad(self):
+        self.cursor.execute(
+            "CREATE TABLE IF NOT EXISTS RESERVA_ACTIVIDAD("
+            "id INTEGER PRIMARY KEY,"
+            "fecha date,"
+            "hora_ingreso time,"
+            "hora_salida time,"
+            "confirmada boolean DEFAULT 0,"
+            "cancelada boolean DEFAULT 0,"
+            "disponible boolean,"
+            "pago_realizado boolean,"
+            "ID_actividad int,"
+            "ID_huesped int ,"
+            "ID_forma_reserva int ,"
+            "FOREIGN KEY (ID_actividad) REFERENCES ACTIVIDAD(id),"
+            "FOREIGN KEY (ID_huesped) REFERENCES HUESPED(id),"
+            "FOREIGN KEY (ID_forma_reserva) REFERENCES FORMA_RESERVA(id))"
+        )
+        self.conexion.commit()
+
+    def InsertarReservaActividad(self, fecha, hora_ingreso, hora_salida, confirmada, cancelada, disponible,
+                                 pago_realizado, ID_actividad, ID_huesped, ID_forma_reserva):
+        self.cursor.execute(
+            "INSERT INTO RESERVA_HABITACION (fecha, hora_ingreso, hora_salida, confirmada, cancelada, disponible, pago_realizado, ID_actividad, ID_huesped, ID_forma_reserva) VALUES (?,?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            (fecha, hora_ingreso, hora_salida, confirmada, cancelada, disponible, pago_realizado, ID_actividad,
+             ID_huesped, ID_forma_reserva))
+        self.conexion.commit()
+
+    def MostrarReservaActividad(self):
+        self.cursor.execute("SELECT * FROM RESERVA_ACTIVIDAD")
+        reserva_actividad = self.cursor.fetchall()
+        return reserva_actividad
+
+    def EditarReservaActividad(self, id, fecha, hora_ingreso, hora_salida, confirmada, cancelada, disponible,
+                               pago_realizado, ID_actividad, ID_huesped, ID_forma_reserva):
+        self.cursor.execute(
+            " UPDATE RESERVA_ACTIVIDAD SET fecha=? , hora_ingreso=?, hora_salida=? , confirmada=?, cancelada=? , disponible=?, confirmada=? , pago_realizado=?, ID_actividad=?, ID_huesped=?, ID_forma_reserva=? WHERE id=?",
+            (fecha, hora_ingreso, hora_salida, confirmada, cancelada, disponible, pago_realizado, ID_actividad,
+             ID_huesped, ID_forma_reserva, id))
+        self.conexion.commit()
+
+    def EliminarReservaActividad(self, id):
+        self.cursor.execute(" DELETE FROM RESERVA_ACTIVDAD WHERE id=?  ", (id))
+        self.conexion.commit()
+
+    ##falta filtro de las reservas de las actividades
+
+    ##CERRAR CONEXION
     def CerrarConexion(self):
         self.cursor.close()
         self.conexion.close()
--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ import sqlite3
 from bdd.bd import Conexion
 import unittest
 
-'''
-class TestHabitacionesDisponibles(unittest.TestCase):
+
+'''class TestHabitacionesDisponibles(unittest.TestCase):
 
     def setUp(self):
         self.nombreBD = "test_HouseHunter.db"
@@ -41,17 +41,19 @@ class TestHabitacionesDisponibles(unittest.TestCase):
         habitacion_editada = self.conexion.MostrarHabitaciones()[0]
 
         self.assertEqual(habitacion_editada[1], precioeditar)
-        self.assertEqual(habitacion_editada[2], capacidadeditar)
-'''
+        self.assertEqual(habitacion_editada[2], capacidadeditar)'''
+
 if __name__ == "__main__":
     print("Bienvenido al sistema")
     nombreBD = "houseHounter.db"
     conexion = Conexion(nombreBD)
     conexion.crearTablaHabitaciones()
+    conexion.crearTablaActividad()
+    conexion.crearTablaHuesped()
     error = 1
     opciones = 0
-    unittest.main()
-    while error!= 0:
+    ##unittest.main()
+    while error != 0:
         try:
             opciones = int(input('''¿Que desea hacer?
                           1- Crear habitacion
@@ -61,18 +63,33 @@ if __name__ == "__main__":
                           5- Buscar habitaciones por filtro
                           6- Crear actividades
                           7- Ver actividades
-                          8- Cancelar actividades
-                          9- Salir del sistema'''))
+                          8- Eliminar actividades
+                          9- Crear huesped
+                          10- Ver huespedes
+                          11- Eliminar huesped
+                          12- Crear reserva
+                          13- Ver reservas
+                          14- Salir del sistema'''))
 
         except ValueError:
             print("La opcion ingresada no es valida")
             opciones = 0
 
         if opciones == 1:
-            precio = float(input("Ingrese el costo del alquiler de la habitacion: "))
-            capacidad = int(input("Ingrese la capacidad de la habitacion"))
-            conexion.insertarHabitacion(precio,capacidad)
-            print("La habitacion fue creada")
+            while True:
+                try:
+                    precio = float(input("Ingrese el costo del alquiler de la habitación: "))
+                    capacidad = int(input("Ingrese la capacidad de la habitación: "))
+
+                    if precio > 0 and capacidad > 0:
+                        conexion.insertarHabitacion(precio, capacidad)
+                        print("La habitación fue creada")
+                        break
+                    else:
+                        print("El precio y la capacidad deben ser valores positivos.")
+                except ValueError:
+                    print("Ingresa valores numéricos válidos para el precio y la capacidad.")
+
 
         elif opciones == 2:
             habitaciones = conexion.MostrarHabitaciones()
@@ -86,46 +103,192 @@ if __name__ == "__main__":
 
             else:
                 print("No hay habitaciones creadas")
+
         elif opciones == 3:
-            id = input("Ingrese el numero de la habitacion a editar")
-            precio = input("Ingrese el precio de la habitacion")
-            capacidad = input("Ingrese la capacidad de la habitacion")
-            conexion.EditarHabitacion(id, precio, capacidad)
-            print("La habitacion fue editada con exito")
+            while True:
+                try:
+                    id = int(input("Ingrese el número de la habitación a editar: "))
+
+                    if id < 0:
+                        print("El ID ingresado no es válido. Debe ser mayor o igual a 0.")
+                        continue
+
+                    conexion.cursor.execute("SELECT id FROM HABITACION WHERE id=?", (id,))
+                    resultado = conexion.cursor.fetchone()
+
+                    if resultado:
+                        precio = float(input("Ingrese el nuevo precio de la habitación: "))
+                        capacidad = int(input("Ingrese la nueva capacidad de la habitación: "))
+
+                        if precio > 0 and 8>= capacidad >= 0:
+                            conexion.EditarHabitacion(id, precio, capacidad)
+                            print("La habitación fue editada con éxito.")
+                            break
+
+                        else:
+                            print("El precio debe ser mayor a cero y la capacidad de 1 a 8 personas.")
+                    else:
+                        print("El ID ingresado no existe en la base de datos.")
+                except ValueError:
+                    print("Ingresa valores válidos para ID, precio y capacidad.")
 
         elif opciones == 4:
-            id = input("Ingrese el id de la habitacion para eliminarla")
-            conexion.EliminarHabitacion(id)
-            print("La habitacion fue eliminada con exito")
+            while True:
+                try:
+                    id = int(input("Ingrese el id de la habitacion para eliminarla"))
+
+                    if id < 0:
+                        print("El ID ingresado no es valido")
+
+                    else:
+                        conexion.cursor.execute("SELECT id FROM HABITACION WHERE id=?", (id,))
+                        resultado = conexion.cursor.fetchone()
+
+                        if resultado:
+                            conexion.EliminarHabitacion(id)
+                            print("La habitacion fue eliminada con éxito")
+                            break
+                        else:
+                            print("El ID ingresado no existe")
+                except ValueError:
+                    print("Error")
+
+
 
         elif opciones == 5:
-            precio_max = float(input("Ingrese el precio máximo que busca en una habitacion"))
-            capacidad_min = int(input("Ingrese la capacidad minima que busca en una habitacion"))
-            conexion.FiltrarHabitaciones(precio_max,capacidad_min)
+
+            while True:
+                try:
+                    precio_max = float(input("Ingrese el precio máximo que busca en una habitación: "))
+
+                    if precio_max <= 0:
+                        print("El precio máximo debe ser mayor que cero.")
+                        continue
+
+                    capacidad_min = int(input("Ingrese la capacidad mínima que busca en una habitación: "))
+
+                    if capacidad_min <= 0:
+                        print("La capacidad mínima debe ser mayor que cero.")
+                        continue
+
+                    conexion.FiltrarHabitaciones(precio_max, capacidad_min)
+                    break
+
+                except ValueError:
+                    print("Ingrese valores válidos para precio máximo y capacidad mínima.")
+
+
+
 
 
         elif opciones == 6:
-            nombre = input("Ingrese el nombre de la actividad: ")
-            tipo = input("Ingrese el tipo de actividad: ")
-            precio = float(input("Ingrese el precio de la actividad: "))
-            fechastr = input("Ingrese la fecha y hora de la actividad con el formato 'YYYY-MM-DD HH:MM:SS': ")
-            try:
-                fecha = datetime.strptime(fechastr, "%Y-%m-%d %H:%M:%S")
-                print("Fecha de la actividad: ", fecha)
-            except ValueError:
-                print("El formato de la fecha ingresada no es valido, use 'YYYY-MM-DD HH:MM:SS' por favor")
-            idioma = input("Ingrese el idioma de la actividad: ")
-            actividadNueva = Actividades(nombre, tipo, precio, fecha, idioma)
+            while True:
+                try:
+                    actividad = input("Ingrese el nombre de la actividad: ")
+                    precio = float(input("Ingrese el precio de la actividad por persona: "))
+
+                    if precio <= 0:
+                        print("El precio de la actividad debe ser mayor que cero.")
+                        continue
+
+                    cupos = int(input("Ingrese los cupos totales de la actividad: "))
+
+                    if cupos <= 0:
+                        print("Los cupos totales deben ser mayores que cero.")
+                        continue
+
+                    cant_reservas = 0
+                    disponible = 1
+                    conexion.InsertarActividad(actividad, precio, cupos, cant_reservas, disponible)
+                    print("Actividad creada con éxito.")
+                    break
+
+                except ValueError:
+                    print("Ingrese valores válidos para precio y cupos.")
+
 
         elif opciones == 7:
-            Actividades.verActividades()
+            actividades = conexion.MostrarActividad()
+
+            if len(actividades) > 0:
+                print("Lista de actividades")
+                for actividad in actividades:
+                    print(
+                        f"Numero: {actividad[0]}, Actividad: {actividad[1]} , Precio: {actividad[2]}, Cupos totales: {actividad[3]}, Inscriptos: {actividad[4]}"
+                    )
+
+            else:
+                print("No hay actividades creadas")
+
         elif opciones == 8:
-            pass
+
+            id = input("Ingrese el numero de la actividad para borrarla")
+            conexion.EliminarActividad(id)
+
+
         elif opciones == 9:
+
+            nombre = input("Ingrese el nombre del huésped: ")
+            apellido = input("Ingrese el apellido del huésped: ")
+
+            while True:
+                try:
+                    dni = int(input("Ingrese el DNI del huésped: "))
+
+                    if len(str(dni)) != 8:
+                        print("El DNI debe tener 8 dígitos.")
+                        continue
+                    break
+
+                except ValueError:
+                    print("Ingrese un número de DNI válido.")
+
+            while True:
+                try:
+                    edad = int(input("Ingrese la edad del huésped: "))
+
+                    if edad <= 16:
+                        print("El huésped debe ser mayor de 16 años.")
+                        continue
+                    break
+
+                except ValueError:
+                    print("Ingrese una edad válida.")
+
+            while True:
+
+                sexo = input("Ingrese el sexo del huésped (M/F): ")
+
+                if sexo not in ["M", "F"]:
+                    print("Ingrese 'M' para masculino o 'F' para femenino.")
+                else:
+                    break
+
+            idioma = input("Ingrese el idioma del huésped: ")
+            nacionalidad = input("Ingrese la nacionalidad del huésped: ")
+
+            conexion.InsertarHuesped(nombre, apellido, dni, edad, sexo, idioma, nacionalidad)
+
+        elif opciones == 10:
+            huespedes = conexion.MostrarHuesped()
+
+            if len(huespedes) > 0:
+                print("Lista de huespedes")
+                for huesped in huespedes:
+                    print(
+                        f"Numero: {huesped[0]}, Nombre: {huesped[1]} , Apellido: {huesped[2]}, Edad: {huesped[4]}, Sexo: {huesped[5]}, Nacionalidad: {huesped[7]}"
+                    )
+
+            else:
+                print("No hay huespedes actualmente")
+
+        elif opciones == 11:
+            id = input("Ingrese el id del huesped para eliminarlo")
+            conexion.EliminarHuesped(id)
+
+        elif opciones == 12:
             print("Usted ha salido del sistema, muchas gracias")
-            error=0
+            error = 0
             conexion.CerrarConexion()
         else:
             print("La opcion ingresada no es valida")
-
-
